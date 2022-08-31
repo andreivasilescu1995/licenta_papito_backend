403804356a00f29d29fe433c7d775e18574136c8
mihai <mihai.voicu842@gmail.com>
2022-08-31 21:37:03 +0300

    crud orders and clients

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'licenta'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', '12345678'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      // database: env('DATABASE_NAME', 'licenta'),
+      database: env("DATABASE_NAME", "papito"),
+      user: env("DATABASE_USERNAME", "root"),
+      // password: env('DATABASE_PASSWORD', '12345678'),
+      password: env("DATABASE_PASSWORD", "123456"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
